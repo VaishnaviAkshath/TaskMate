db5cd8af4f4bc9a54e2d508e09df4950f3f86758
vaishnavi.shetty111@gmail.com <vaishnavi.shetty111@gmail.com>
2025-10-16 11:06:49 +0530

    16th oct TaskMate completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Taskform from "./Components/Taskform"
 import Tasklist from "./Components/Tasklist"
 import Progresstracker from "./Components/Progresstracker"
+import { useEffect, useState } from "react"
 export default function App() {
+  const [tasks, setTasks] = useState([]);  // handles mutiple tasks
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  });
+  const addTask = (task) => {
+    setTasks([...tasks,task]);
+  }
   return (
     <div>
       <h1>TaskMate</h1>
       <p>My friendly Task Manager</p>
-      <Taskform />
+      <Taskform addTask = {addTask}/>
       <Tasklist />
       <Progresstracker />
       <button>Clear All Tasks</button>
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,20 +1,37 @@
-export default function Taskform() {
+import { useState } from "react"
+
+export default function Taskform({addTask}) {
+    const[task, setTask] = useState('');
+    const[priority, setPriority] = useState('Medium');
+    const[category, setCategory] = useState('General');
+
+    const handlesubmit = (e) => {
+        e.preventDefault(); // does not refresh the page upon submit
+        addTask({text: task, priority, category, completed: false});
+
+        //reset
+        setTask('');
+        setPriority('medium');
+        setCategory('general');
+    }
     return (
         <div>
             {/* <h1>TaskForm Component</h1> */}
-            <form>
+            <form onSubmit={handlesubmit}>
                 <div>
-                    <input type="text" placeholder="Enter the task"/>
+                    <input type="text" placeholder="Enter the task" value={task}
+                    onChange={(e) => setTask(e.target.value)}/>
                     <button type="submit">Add Task</button>
+                    <h1>{task} {priority} {category}</h1>
                 </div>
                 <div>
-                    <select>
+                    <select value={priority} onChange={(e) => setPriority(e.target.value)}>
                         <option value="High">High</option>
                         <option value="Medium">Medium</option>
                         <option value="Low">Low</option>
                     </select>
 
-                    <select>
+                    <select value={category} onChange={(e) => setCategory(e.target.value)}>
                         <option value="General">General</option>
                         <option value="Work">Work</option>
                         <option value="Personal">Personal</option>
